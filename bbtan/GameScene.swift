60e9aa80c27aea822bc5c1248e20353688ac5967
Milan Suk <--global>
2022-09-26 16:36:22 +0200

    feat: obstacle points

--- bbtan/GameScene.swift
+++ bbtan/GameScene.swift
@@ -6,14 +6,17 @@
 //
 
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
-    let ball = createBall()
+    var obstaclePoints = 50
     
-    private static func createBall() -> SKSpriteNode {
+    private func createBall() -> SKSpriteNode {
         let ballRadius = 15 as CGFloat
+        
         let ball = SKSpriteNode(imageNamed: "ball")
+        ball.name = "ball"
+        ball.position = CGPoint(x: frame.midX, y: frame.midY)
+        
         ball.physicsBody = SKPhysicsBody(circleOfRadius: ballRadius)
         ball.physicsBody?.friction = 0.0
         ball.physicsBody?.restitution = 1.0
@@ -21,6 +24,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         ball.physicsBody?.linearDamping = 0.0
         ball.physicsBody?.angularDamping = 0.0
         ball.physicsBody?.allowsRotation = false
+        ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
         
         return ball
     }
@@ -30,11 +34,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let obstacleSize = CGSize(width: obstacleEdgeSize, height: obstacleEdgeSize)
         
         let obstacle = SKSpriteNode(color: .red, size: obstacleSize)
-        obstacle.position = CGPoint(x: frame.midX + 10, y: frame.midY - 20)
+        obstacle.name = "obstacle"
+        obstacle.position = CGPoint(x: frame.midX + 10, y: frame.midY - 10)
         obstacle.physicsBody = SKPhysicsBody(rectangleOf: obstacleSize)
         obstacle.physicsBody?.isDynamic = false
         
         return obstacle
+    }
+    
+    private func createObstaclePoints() -> SKLabelNode {
+        let obstaclePoints = SKLabelNode(text: String(obstaclePoints))
+        obstaclePoints.name = "obstaclePoints"
+        obstaclePoints.position = CGPoint(x: frame.midX + 10, y: frame.midY - 20)
+        return obstaclePoints
     }
     
     override func didMove(to view: SKView) {
@@ -42,13 +54,27 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         physicsBody?.restitution = 1.0
         physicsBody?.friction = 0.0
         
-        ball.position = CGPoint(x: frame.midX, y: frame.midY)
-        addChild(ball)
+        physicsWorld.contactDelegate = self
+        
+        addChild(createBall())
         addChild(createObstacle())
+        addChild(createObstaclePoints())
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        ball.physicsBody?.applyImpulse(CGVector(dx: 40, dy: 40))
+        if let ball = childNode(withName: "ball") {
+            ball.physicsBody?.applyImpulse(CGVector(dx: 40, dy: 40))
+        }
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        guard let nodeA = contact.bodyA.node else { return }
+        guard let nodeB = contact.bodyB.node else { return }
+        
+        if nodeA.name == "obstacle" || nodeB.name == "obstacle" {
+            obstaclePoints -= 1
+            (childNode(withName: "obstaclePoints") as? SKLabelNode)?.text = String(obstaclePoints - 1)
+        }
     }
     
     override func update(_ currentTime: TimeInterval) {
